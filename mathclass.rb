d34efdfe97164ae3f506fdd3e2e122e5a3b9087c
John Mansell <jmansell90@gmail.com>
2020-07-24 17:36:27 -0500

    Added test class (First try, just googled as I went along.  Lets cross our fingers)

---
+++ mathclass.rb
@@ -0,0 +1,15 @@
+#!/usr/local/bin/ruby
+#MathClass ruby test to test sum and difference between two numbers
+class MathClass
+    #Class to add two numbers
+    def add(num1, num2)
+        total = num1 + num2
+        puts total
+    end
+
+    #Class to set diff between numbers
+    def diff(num1, num2)
+        total = num1 - num2
+        puts (total).abs
+    end
+end
