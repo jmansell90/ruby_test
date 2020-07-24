c21dcfa4668a41be8deed3acd60b21d341e1bde1
John Mansell <jmansell90@gmail.com>
2020-07-24 17:30:39 -0500

    Added rspec test

---
+++ spec/math_class_spec.rb
@@ -0,0 +1,24 @@
+require "MathClass"
+describe ".sum" do
+    context "given 2, 6" do
+        it "returns 8" do
+            expect(MathClass.sum(2, 6)).to eql(8)
+        end
+    end
+    context "given 24, 15" do
+        it "returns 39" do
+            expect(MatchClass.sum(24, 15)).to eql(39)
+        end
+    end
+describe ".diff" do
+    context "given 17, 12" do
+        it "returns 5" do
+            expect(MathClass.diff(17, 12)).to eql(5)
+        end
+    end
+    context "given 23, 86" do
+        it "returns 63" do
+            expect(MatchClass.sum(23, 86)).to eql(63)
+        end
+    end
+end
