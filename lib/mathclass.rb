19126af53c0eec9cc34ee2d40c413f48cc52ade4
John Mansell <jmansell90@gmail.com>
2020-07-28 10:35:17 -0500

    Finally have a working test

--- lib/mathclass.rb
+++ lib/mathclass.rb
@@ -1,6 +1,9 @@
 #!/usr/local/bin/ruby
 #MathClass ruby test to test sum and difference between two numbers
 class MathClass
+    #Init
+    def new(*args)
+
     #Class to add two numbers
     def add(num1, num2)
         total = num1 + num2
--- spec/mathclass_spec.rb
+++ spec/mathclass_spec.rb
@@ -1,25 +1,27 @@
-require "lib/mathclass.rb"
+require 'mathclass.rb'
 describe ".sum" do
+    mathclass = MathClass.new
     context "given 2, 6" do
         it "returns 8" do
-            expect(MathClass.sum(2, 6)).to eql(8)
+            expect(mathclass.add(2, 6)).to eql(8)
         end
     end
     context "given 24, 15" do
         it "returns 39" do
-            expect(MatchClass.sum(24, 15)).to eql(39)
+            expect(mathclass.add(24, 15)).to eql(39)
         end
     end
 end
 describe ".diff" do
+    mathclass = MathClass.new
     context "given 17, 12" do
         it "returns 5" do
-            expect(MathClass.diff(17, 12)).to eql(5)
+            expect(mathclass.diff(17, 12)).to eql(5)
         end
     end
     context "given 23, 86" do
         it "returns 63" do
-            expect(MatchClass.sum(23, 86)).to eql(63)
+            expect(mathclass.diff(23, 86)).to eql(63)
         end
     end
 end
