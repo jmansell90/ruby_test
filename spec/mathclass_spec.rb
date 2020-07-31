a6ee8d34e28d965c398461c6388bc97905ff1e2e
John Mansell <jmansell90@gmail.com>
2020-07-31 12:08:00 -0500

    Sleep deprivation makes it hard to see parenthesis

--- spec/mathclass_spec.rb
+++ spec/mathclass_spec.rb
@@ -3,12 +3,12 @@ describe "2 and 6" do
     mathclass = MathClass.new(2, 6)
     context "Adding 2 and 6" do
         it "returns 8" do
-            expect(mathclass.sum.to eql(8)
+            expect(mathclass.sum.to eql(8))
         end
     end
     context "Difference between 2 and 6" do
         it "returns 4" do
-            expect(mathclass.diff.to eql(4)
+            expect(mathclass.diff.to eql(4))
         end
     end
 end
@@ -16,12 +16,12 @@ describe "23 and 86" do
     mathclass = MathClass.new(23,86)
     context "adding 23 and 86" do
         it "returns 109" do
-            expect(mathclass.sum.to eql(109)
+            expect(mathclass.sum.to eql(109))
         end
     end
     context "Difference between 23 and 86" do
         it "returns 63" do
-            expect(mathclass.diff.to eql(63)
+            expect(mathclass.diff.to eql(63))
         end
     end
 end
