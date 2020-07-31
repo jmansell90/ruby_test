ceac2e61ab9e23ab2c64b49d1246d05a278c3a01
John Mansell <jmansell90@gmail.com>
2020-07-31 12:42:00 -0500

    I'm thinking it didn't blank the class on reinitialization

--- spec/mathclass_spec.rb
+++ spec/mathclass_spec.rb
@@ -1,27 +1,27 @@
 require 'mathclass.rb'
 describe "2 and 6" do
-    mathclass = MathClass.new(2, 6)
+    mathclassa = MathClass.new(2, 6)
     context "Added" do
         it "returns 8" do
-            expect(mathclass.sum).to eql(8)
+            expect(mathclassa.sum).to eql(8)
         end
     end
     context "Difference" do
         it "returns 4" do
-            expect(mathclass.diff).to eql(4)
+            expect(mathclassa.diff).to eql(4)
         end
     end
 end
 describe "23 and 86" do
-    mathclass = MathClass.new(23,86)
+    mathclassb = MathClass.new(23,86)
     context "added" do
         it "returns 109" do
-            expect(mathclass.sum).to eql(109)
+            expect(mathclassb.sum).to eql(109)
         end
     end
     context "Difference" do
         it "returns 63" do
-            expect(mathclass.diff).to eql(63)
+            expect(mathclassb.diff).to eql(63)
         end
     end
 end
