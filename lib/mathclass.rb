3a9b5c83069734ea0da02b645480d6637afc3983
John Mansell <jmansell90@gmail.com>
2020-07-31 12:50:32 -0500

    What in all that is holy is an instance variable?

--- lib/mathclass.rb
+++ lib/mathclass.rb
@@ -1,21 +1,18 @@
 #!/usr/bin/ruby
 #MathClass ruby test to test sum and difference between two numbers
 class MathClass
-    $cnum1 = 0
-    $cnum2 = 0
-
     def initialize(num1, num2)
-        $cnum1 = num1
-        $cnum2 = num2
+        @num1 = num1
+        @num2 = num2
     end
 
     #Class to add two numbers
     def sum
-        return $cnum1+$cnum2
+        return @num1+@num2
     end
 
     #Class to set diff between numbers
     def diff
-        return ($cnum1-$cnum2).abs
+        return (@num1-@num2).abs
     end
 end
