263075554a3ab88f776f14305d621709e0a9f67a
John Mansell <jmansell90@gmail.com>
2020-07-31 12:46:28 -0500

    Changed to global variables

--- lib/mathclass.rb
+++ lib/mathclass.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/ruby
 #MathClass ruby test to test sum and difference between two numbers
 class MathClass
-    cnum1 = 0
-    cnum2 = 0
+    $cnum1 = 0
+    $cnum2 = 0
 
     def initialize(num1, num2)
-        cnum1 = num1
-        cnum2 = num2
+        $cnum1 = num1
+        $cnum2 = num2
     end
 
     #Class to add two numbers
     def sum
-        return cnum1+cnum2
+        return $cnum1+$cnum2
     end
 
     #Class to set diff between numbers
     def diff
-        return (cnum1-cnum2).abs
+        return ($cnum1-$cnum2).abs
     end
 end
